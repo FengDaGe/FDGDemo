d07da2543e64bee4547a10b35402b65fa81823b2
赵高峰 <zhaogaofeng@zhaogaofengdeMacBook-Pro.local>
2017-03-18 20:45:26 +0800

    init

---
+++ FDGDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FDGDemo
+//
+//  Created by 赵高峰 on 2017/3/18.
+//  Copyright © 2017年 赵高峰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FDGDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FDGDemo
+//
+//  Created by 赵高峰 on 2017/3/18.
+//  Copyright © 2017年 赵高峰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
